315682874cf8fad970f4e6b0cb928a080e868bab
kiran s <nanditha1669@gmail.com>
2025-11-27 14:38:08 +0530

    first

---
+++ react31/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+
+class Footer extends React.Component{
+
+    render(){
+        return <div>
+                   <h2>Footer-class component</h2>
+               </div>
+    }
+}
+export default Footer;
