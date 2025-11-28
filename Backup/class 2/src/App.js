62c4cbe8951abf5cb731f4ca44655c628154d6d2
kiran s <nanditha1669@gmail.com>
2025-11-28 12:13:21 +0530

    first

---
+++ Backup/class 2/src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+let App=()=>{
+
+  return <div>
+            <h1>App Component</h1>
+            <hr />
+            <Header/>
+            <Footer/>
+         </div>
+}
+export default App;
---
+++ Backup/class 2/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+
+class Header extends React.Component{
+
+    render(){
+        return <div>
+                   <h2>Header-class component</h2>
+               </div>
+    }
+}
+export default Header;
